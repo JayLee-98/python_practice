8ba525494ae9a60572a84764c11fc4624ce69da3
jaylee <jayginwoolee98@gmail.com>
2024-07-01 13:09:50 +0900

    variable, operator

---
+++ 3. operator/3. simple_calculation.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 절댓값
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12))
+print(min(5, 12))
+print(round(3.14)) # 반올림
+print(round(4.99))
+
+from math import *
+print(floor(4.99)) # 무조건 내림
+print(ceil(3.14)) # 무조건 올림
+print(sqrt(16)) # squar root 제곱근
